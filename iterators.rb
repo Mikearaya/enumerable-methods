240d670b6a2a8d4a39da786811583ae142d2f9f1
mikearaya <mikaelaraya12@gmail.com>
2020-04-24 18:48:16 +0300

    Create my_each_with_index method

--- iterators.rb
+++ iterators.rb
@@ -12,13 +12,21 @@ module Enumerable
       index += 1
     end
   end
+
+  def my_each_with_index
+    return unless block_given?
+
+    index = 0
+    while index < size
+      yield(self[index], index)
+      index += 1
+    end
+  end
 end
 
 my_hash = {
   name: 'Mikael Araya',
   phone: '0912333333'
 }
-
-[1, 2, 3, 4, 5].my_each { |index| puts "index = #{index}" }
 
 my_hash.my_each { |index, value| puts "index = #{index} value => #{value}" }
