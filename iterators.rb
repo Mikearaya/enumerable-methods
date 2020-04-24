457594556e6fe7f904277867446ed8ef20f08ce2
mikearaya <mikaelaraya12@gmail.com>
2020-04-24 18:05:24 +0300

    Refactor my_each method

--- iterators.rb
+++ iterators.rb
@@ -3,18 +3,22 @@ module Enumerable
     return unless block_given?
 
     index = 0
-    if is_a?(Hash)
-      keys = self.keys
-      while index < keys.size
-        yield(keys[index], self[keys[index]])
-        index += 1
-      end
-    else
+    while index < size
 
-      while index < size
-        yield(self[index])
-        index += 1
+      case self.class.name
+      when 'Hash' then yield(keys[index], self[keys[index]])
+      when 'Array' then yield(self[index])
       end
+      index += 1
     end
   end
 end
+
+my_hash = {
+  name: 'Mikael Araya',
+  phone: '0912333333'
+}
+
+[1, 2, 3, 4, 5].my_each { |index| puts "index = #{index}" }
+
+my_hash.my_each { |index, value| puts "index = #{index} value => #{value}" }
