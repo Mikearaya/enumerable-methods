0538432a775b827576b21319c47ee2fef2fbd524
mikearaya <mikaelaraya12@gmail.com>
2020-05-09 19:14:46 +0300

    Add missing examples

--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -2,60 +2,68 @@ require './iterators.rb'
 describe 'Iterators' do
   let(:number) { [1, 2, 3, 4, 2] }
   let(:string) { %w[cat bear rat] }
-  context '#my_all?' do
-    it 'Return true when no block is given' do
+  context '#my_all? should Return' do
+    it 'true when no block is given' do
       expect(number.my_all?).to eq true
     end
 
-    it 'Return false if all checks do not pass' do
+    it 'false when no block is given and one element is falsey' do
+      expect([false, 1, 2, 3].my_all?).not_to eq true
+    end
+
+    it 'false if all checks do not pass' do
       expect(number.my_all? { |a| a > 3 }).not_to eq true
     end
 
-    it 'Return true when all checks pass' do
+    it 'true when all checks pass' do
       expect(number.my_all? { |a| a > 0 }).to eq true
     end
-    it 'Return true when all elements belong to same datatype' do
+    it 'true when all elements belong to same datatype' do
       expect(number.my_all?(Numeric)).to eq true
     end
 
-    it 'Return false when some elements dont belong to same datatype' do
+    it 'false when some elements dont belong to same datatype' do
       expect(number.my_all?(String)).not_to eq true
     end
 
-    it 'Return true when all elements match pattern' do
+    it 'true when all elements match pattern' do
       expect(string.my_all?(/a/i)).to eq true
     end
 
-    it 'Return false when some elements do not match pattern' do
+    it 'false when some elements do not match pattern' do
       expect(string.my_all?(/t/i)).not_to eq true
     end
   end
 
-  context '#my_any?' do
-    it 'Return true when no block is given' do
+  context '#my_any? should return' do
+    it 'true when no block is given' do
       expect(number.my_any?).to eq true
     end
 
-    it 'Return if any of checks pass' do
+    it 'false when no block is given and none is truthy' do
+      expect([nil, false].my_any?).not_to eq true
+    end
+
+    it 'if any of checks pass' do
       expect(number.my_any? { |a| a > 3 }).to eq true
     end
 
-    it 'Return false when all checks fail' do
+    it 'false when all checks fail' do
       expect(number.my_any? { |a| a < 0 }).to eq false
     end
-    it 'Return true when some elements belong to same datatype' do
+    it 'true when some elements belong to same datatype' do
       expect(number.my_any?(Numeric)).to eq true
     end
 
-    it 'Return false when all elements dont belong to same datatype' do
+    it 'false when all elements dont belong to same datatype' do
       expect(number.my_any?(String)).not_to eq true
     end
 
-    it 'Return true when any elements match pattern' do
+    it 'true when any elements match pattern' do
       expect(string.my_any?(/c/i)).to eq true
     end
 
-    it 'Return false when no element match pattern' do
+    it 'false when no element match pattern' do
       expect(string.my_any?(/y/i)).not_to eq true
     end
   end
@@ -75,7 +83,7 @@ describe 'Iterators' do
 
   context '#my_filter should Return ' do
     it 'items that match block expression' do
-      expect(number.my_filter(&:even?)).to all(be_even)
+      expect(number.my_filter(&:even?)).to match_array([2, 4, 2])
     end
 
     it 'enumerator when no block is given' do
